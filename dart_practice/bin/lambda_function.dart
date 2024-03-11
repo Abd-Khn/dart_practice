03438ba6f78b61e77c00f975d15ce0d3d4973017
abdullah <ak_fury7@yahoo.com>
2024-03-11 09:48:11 +0500

    Lambda Function

---
+++ dart_practice/bin/lambda_function.dart
@@ -0,0 +1,24 @@
+
+void main(){
+
+  void twoNumbers(int a, int b){
+    int sum = a + b;
+    print("Sum of $a and $b is $sum");
+  }
+
+  twoNumbers(5, 5);
+
+   int mulNumber (int num){
+    return num * num;
+
+  }
+  print(mulNumber(3));
+
+
+}
+
+
+void addNumbers(int a, int b){
+  int sum = a + b;
+  print("Sum of $a and $b is $sum");
+}
