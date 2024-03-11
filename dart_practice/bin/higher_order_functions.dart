55828476e7835bcdcd8129db099b9356bf24d48d
abdullah <ak_fury7@yahoo.com>
2024-03-11 10:02:07 +0500

    Higher Order Functions updated

---
+++ dart_practice/bin/higher_order_functions.dart
@@ -0,0 +1,23 @@
+void main() {
+
+  addNumbers(a, b){
+  print(a + b);
+}
+  someOtherFunction("Hello", addNumbers);
+
+
+  var myFunc = taskToPerform();
+  print(myFunc(10));
+}
+
+void someOtherFunction(String message, Function myFunction) {       // Higher-Order Function
+
+  print(message);
+  myFunction(2, 4);
+}
+
+Function taskToPerform() {       // Higher-Order Function
+
+  multiplyFour(int number) => number * 4;
+  return multiplyFour;
+}
